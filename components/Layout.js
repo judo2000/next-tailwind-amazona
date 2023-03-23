6df0b7df194f5d4871415a83d01420de06562b6b
Stephen Moore <judo2000@comcast.net>
2023-03-23 14:23:39 -0600

    Implement add to cart functionality with  context.

--- components/Layout.js
+++ components/Layout.js
@@ -1,7 +1,11 @@
+import { Store } from '@/utils/Store';
 import Head from 'next/head';
 import Link from 'next/link';
+import { useContext } from 'react';
 
 function Layout({ title, children }) {
+  const { state, dispatch } = useContext(Store);
+  const { cart } = state;
   return (
     <>
       <Head>
@@ -19,6 +23,11 @@ function Layout({ title, children }) {
             <div>
               <Link href="/cart" className="p-2">
                 Cart
+                {cart.cartItems.length > 0 && (
+                  <span className="ml-1 rounded-full bg-red-600 px-2 py-1 text-xs font-bold text-white">
+                    {cart.cartItems.reduce((a, c) => a + c.quantity, 0)}
+                  </span>
+                )}
               </Link>
               <Link href="/login" className="p-2">
                 Login
--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,31 +1,51 @@
 import Layout from '@/components/Layout';
 import data from '@/utils/data';
+import { Store } from '@/utils/Store';
 import Image from 'next/image';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
+import { useContext } from 'react';
 
 const ProductScreen = () => {
+  const { state, dispatch } = useContext(Store);
   const { query } = useRouter();
   const { slug } = query;
   const product = data.products.find((x) => x.slug === slug);
 
   if (!product) {
-    <div>Product Not Found</div>;
+    return <div>Product Not Found</div>;
   }
+  console.log(product);
+
+  const addToCartHandler = () => {
+    const itemExists = state.cart.cartItems.find(
+      (x) => x.slug === product.slug
+    );
+    const quantity = itemExists ? itemExists.quantity + 1 : 1;
+
+    if (product.countInStock < quantity) {
+      alert('Sorry, Product is out of stock');
+      return;
+    }
+    dispatch({ type: 'CART_ADD_ITEM', payload: { ...product, quantity } });
+  };
+
   return (
-    <Layout title={product.name}>
+    <Layout title={product?.name}>
       <div className="py-2">
         <Link href="/">back to products</Link>
       </div>
       <div className="grid md:grid-cols-4 md:gap-3">
         <div className="md:col-span-2">
-          <Image
-            src={product.image}
-            alt={product.name}
-            width={640}
-            height={640}
-            responsive
-          />
+          {product?.image && (
+            <Image
+              src={product.image}
+              alt={product.name}
+              width={640}
+              height={640}
+              responsive
+            />
+          )}
         </div>
         <div>
           <ul>
@@ -52,7 +72,12 @@ const ProductScreen = () => {
                 {product.countInStock > 0 ? 'In stock' : 'Out of stock'}
               </div>
             </div>
-            <button className="primary-button w-full">Add to cart</button>
+            <button
+              className="primary-button w-full"
+              onClick={addToCartHandler}
+            >
+              Add to cart
+            </button>
           </div>
         </div>
       </div>
