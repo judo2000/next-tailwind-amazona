712c3b687dd992a4c3fc65c08a9607145e04513f
Stephen Moore <judo2000@comcast.net>
2023-03-24 16:39:49 -0600

    Implement login functionality with NextAuth

--- components/Layout.js
+++ components/Layout.js
@@ -1,9 +1,14 @@
 import { Store } from '@/utils/Store';
 import Head from 'next/head';
 import Link from 'next/link';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import { useContext, useEffect, useState } from 'react';
+import { useSession } from 'next-auth/react';
 
 function Layout({ title, children }) {
+  const { status, data: session } = useSession();
+
   const { state } = useContext(Store);
   const { cart } = state;
   const [cartItemsCount, setCartItemsCount] = useState(0);
@@ -19,6 +24,7 @@ function Layout({ title, children }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <ToastContainer position="top-center" limit={1} />
       <div className="flex min-h-screen flex-col justify-between">
         <header>
           <nav className="flex h-12 items-center justify-between px-4 shadow-md">
@@ -34,9 +40,14 @@ function Layout({ title, children }) {
                   </span>
                 )}
               </Link>
-              <Link href="/login" className="p-2">
-                Login
-              </Link>
+
+              {status === 'loading' ? (
+                'Loading'
+              ) : session?.user ? (
+                session.user.name
+              ) : (
+                <Link href="/login">Login</Link>
+              )}
             </div>
           </nav>
         </header>
